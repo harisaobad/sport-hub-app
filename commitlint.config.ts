78e48b2760fb659bfc197470c3ce404ec25a0660
harisaobad <harisaobad2@gmail.com>
2025-05-27 18:06:21 +0200

    ci: added husky and commitlint

---
+++ commitlint.config.ts
@@ -0,0 +1,7 @@
+import { RuleConfigSeverity, type UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"]
+}
+
+export default Configuration;
